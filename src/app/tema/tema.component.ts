214a319bc8e7b99ebeb72edada2d3ef0907bdd8d
RIVELINO SANTOS <80266863+ri-ve@users.noreply.github.com>
2021-06-03 14:13:15 -0300

    serviceTema

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/Tema.service';
+
 
 @Component({
   selector: 'app-tema',
@@ -8,15 +11,35 @@ import { environment } from 'src/environments/environment.prod';
   styleUrls: ['./tema.component.css']
 })
 export class TemaComponent implements OnInit {
+  tema:Tema =new  Tema()
+  listaTemas: Tema[]
 
   constructor(
-    private router: Router
+    private router: Router,
+    private temaService:TemaService
   ) { }
 
   ngOnInit() {
    if (environment.token == ''){
         this.router.navigate(['/entrar'])
       }
+
+      this.findAllTemas()
+    }
+
+
+    findAllTemas(){
+      this.temaService.getAllTema().subscribe((resp:Tema[])=>{
+        this.listaTemas = resp
+      })
+    }
+    cadastrar(){
+      this.temaService.postTema(this.tema).subscribe((resp:Tema)=>{
+        this.tema= resp
+        alert('Tema cadastrado com sucesso!')
+        this.findAllTemas()
+        this.tema = new Tema()
+      })
     }
   }
 
